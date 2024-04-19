ddd42cfc5b22a5fba5ef753948d0ad30eefef659
Marta <martuki8tuki@gmail.com>
2024-04-19 12:10:08 +0200

    Test 7 aplicar diferent amplada dropdown

--- components/DropdownGender.vue
+++ components/DropdownGender.vue
@@ -11,14 +11,8 @@ const items = [
 </script>
 
 <template>
-  <UDropdown :items="items" :popper="{ placement: 'bottom-start'}" class="custom-dropdown">
+  <UDropdown :items="items" :popper="{ placement: 'bottom-start'}" style="width: w-96;">
     <UButton color="white" label="Choose the patient gener" trailing-icon="i-heroicons-chevron-down-20-solid" />
   </UDropdown>
 </template>
-
-<style>
-.custom-dropdown {
-  width: w-96;
-}
-</style>
 
