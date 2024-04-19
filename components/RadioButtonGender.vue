88e4748490667c295ab4a6209b96cbd234345b6d
Marta <martuki8tuki@gmail.com>
2024-04-19 12:24:16 +0200

    idem2

--- components/RadioButtonGender.vue
+++ components/RadioButtonGender.vue
@@ -11,7 +11,6 @@ const selected = ref('female')
 </script>
 
 <template>
-    Select the patient gender
-  <URadioGroup v-model="selected" legend="Choose something" :options="options" />
+  <URadioGroup v-model="selected" legend="Select the patient gender" :options="options" />
 </template>
 
