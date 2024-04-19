fa10d4895611a95371f095694b2fe2bce56e0ff1
Marta <martuki8tuki@gmail.com>
2024-04-19 11:29:01 +0200

    Gender dropdown

--- components/Dropdown.vue
+++ components/Dropdown.vue
@@ -1,39 +1,18 @@
 <script setup lang="ts">
 const items = [
   [{
-    label: 'Profile',
-    avatar: {
-      src: 'https://avatars.githubusercontent.com/u/739984?v=4'
-    }
-  }], [{
-    label: 'Edit',
-    icon: 'i-heroicons-pencil-square-20-solid',
-    shortcuts: ['E'],
-    click: () => {
-      console.log('Edit')
-    }
-  }, {
-    label: 'Duplicate',
-    icon: 'i-heroicons-document-duplicate-20-solid',
-    shortcuts: ['D'],
-    disabled: true
-  }], [{
-    label: 'Archive',
+    label: 'Female',
     icon: 'i-heroicons-archive-box-20-solid'
   }, {
-    label: 'Move',
+    label: 'Male',
     icon: 'i-heroicons-arrow-right-circle-20-solid'
-  }], [{
-    label: 'Delete',
-    icon: 'i-heroicons-trash-20-solid',
-    shortcuts: ['⌘', 'D']
   }]
 ]
 </script>
 
 <template>
   <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
-    <UButton color="white" label="Options" trailing-icon="i-heroicons-chevron-down-20-solid" />
+    <UButton color="white" label="Choose the patient gener" trailing-icon="i-heroicons-chevron-down-20-solid" />
   </UDropdown>
 </template>
 
