271db16ba6c10dad0762e7dc6c23bd2593be6346
Marta <martuki8tuki@gmail.com>
2024-04-19 12:10:47 +0200

    rollback del test 7

--- components/DropdownGender.vue
+++ components/DropdownGender.vue
@@ -11,8 +11,14 @@ const items = [
 </script>
 
 <template>
-  <UDropdown :items="items" :popper="{ placement: 'bottom-start'}" style="width: w-96;">
+  <UDropdown :items="items" :popper="{ placement: 'bottom-start'}" class="custom-dropdown">
     <UButton color="white" label="Choose the patient gener" trailing-icon="i-heroicons-chevron-down-20-solid" />
   </UDropdown>
 </template>
+
+<style>
+.custom-dropdown {
+  width: w-60;
+}
+</style>
 
