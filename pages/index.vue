271c223026367885357ca697ef5219fdde0c764d
Marta <martuki8tuki@gmail.com>
2024-04-25 20:43:12 +0200

    Margins-Paddings-BreadCrumb
    Falta revisar el canvi de colors del breadcrumb + centrar la template

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,14 @@
 <template>
-    <DropdownGender/>
-    <RadioButtonGender/>
+
+    <div class="m-8 ...">
+        <BreadCrumb/>
+    </div>
+
+    <div class="m-16 ...">
+        <RadioButtonGender/>
+        <div class="pt-6 ...">
+            <DropdownGender/>
+        </div>
+    </div>
+    
 </template>
