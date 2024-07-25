86d719fb75ac14c4f573afca22f0bbef3c271d4b
Marta <martuki8tuki@gmail.com>
2024-07-25 20:55:21 +0200

    intent d'ajust breadcrumb centrat fallit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div> <!--contenedor-->
+    <div > <!--contenedor-->
         <BreadCrumb>
         </BreadCrumb>
         <main class="flex flex-col w-full h-[100dvh]"> <!--responsive-->
--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,13 @@
         <RadioButtonGender/>
         <div class="pt-6 ...">
             <DropdownAge/>
+        </div>
+        <div>
             <UButton color="red">
                 Pepe
             </UButton>
         </div>
+
     </div>
     
 </template>
