4328dce745adcb725960a67f66fb6399be588162
Marta <martuki8tuki@gmail.com>
2024-07-30 21:27:57 +0200

    Intent d'afegir header i millora del breadcrumb
    FALLIT - Tinc error al breadcrumb no bloquejant i el header no consegueixo que es visualitzi

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,14 @@
 <template>
-
+    <div>
+        <MainHeader>
+            
+        </MainHeader>
+    </div>
     <div > <!--contenedor-->
         <BreadCrumb>
         </BreadCrumb>
         <main class="flex flex-col w-full h-[100dvh]"> <!--responsive-->
             <slot> <!--contenido de todas las pg-->
-
             </slot>
         </main>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,16 @@
 <template>
 
     <div class="m-16 ...">
+
         <RadioButtonGender/>
         <div class="pt-6 ...">
             <DropdownAge/>
         </div>
         <div>
             <UButton color="red">
-                Pepe
+                Search
             </UButton>
         </div>
-
     </div>
     
 </template>
