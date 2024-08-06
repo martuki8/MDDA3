b1e1a162db98a45ed092e9e38fc04d3304c8a7a8
Marta <martuki8tuki@gmail.com>
2024-08-06 20:49:51 +0200

    BaseHeader component is Ugly Header
    He componetitzat el header lleig que havia posat al layout directe

---
+++ components/BaseHeader.vue
@@ -0,0 +1,6 @@
+<template>
+    <header class="border-b-2 flex justify-center py-4 px-4">
+        <NuxtLink class="mr-4" to="/">Home</NuxtLink>
+        <NuxtLink to="https://github.com/martuki8/MDDA3">GitHub</NuxtLink>
+    </header>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,6 @@
 <template>
     <div>
-        <header class="border-b-2 flex justify-center py-4 px-4">
-            <NuxtLink class="mr-4" to="/">Home</NuxtLink>
-            <NuxtLink to="https://github.com/martuki8/MDDA3">GitHub</NuxtLink>
-        </header>
+        <BaseHeader></BaseHeader>
     </div>
     <div > <!--contenedor-->
         <BreadCrumb class="flex justify-center py-10 px-4">
