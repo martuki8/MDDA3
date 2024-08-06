4b0cc5633fc031664abf5d852872cc4dd1202dce
Marta <martuki8tuki@gmail.com>
2024-08-06 20:28:46 +0200

    GitHub link in the header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <header class="border-b-2 flex justify-center py-4 px-4">
-            <NuxtLink to="/">Home</NuxtLink>
-
+            <NuxtLink class="mr-4" to="/">Home</NuxtLink>
+            <NuxtLink to="https://github.com/martuki8/MDDA3">GitHub</NuxtLink>
         </header>
     </div>
     <div > <!--contenedor-->
