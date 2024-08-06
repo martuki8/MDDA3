3608c95037921f2d0360a245c59aeb04c6db2704
Marta <martuki8tuki@gmail.com>
2024-08-06 20:26:10 +0200

    Ugly Header plus justifications and paddings
    he conseguit afegir al layout una mena de headr amb link a la home i justificacions al centre. També paddings verticals i horitzontals

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
-        <MainHeader>
-            
-        </MainHeader>
+        <header class="border-b-2 flex justify-center py-4 px-4">
+            <NuxtLink to="/">Home</NuxtLink>
+
+        </header>
     </div>
     <div > <!--contenedor-->
-        <BreadCrumb>
+        <BreadCrumb class="flex justify-center py-10 px-4">
         </BreadCrumb>
         <main class="flex flex-col w-full h-[100dvh]"> <!--responsive-->
             <slot> <!--contenido de todas las pg-->
