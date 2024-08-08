ed6733c3dc29a18dc9503f7926999b8105388cd4
Marta <martuki8tuki@gmail.com>
2024-08-08 19:03:14 +0200

    Relocation index layout

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,12 @@
 
     <div class="m-2 flex justify-center">
         <RadioButtonGender class="px-10"/>
-        <div class="pt-6 flex justify-center px-10">
-            <DropdownAge/>
-        </div>
-
     </div>
+
+    <div class="pt-6 flex justify-center px-10">
+            <DropdownAge/>
+    </div>
+    
     <div class="flex justify-center py-6">
         <UButton color="blue" class="flex justify-center py-2 px-4">
             Search
