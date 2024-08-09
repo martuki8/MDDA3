068565c23d534bed9b8398e93d481b24dc3526db
Marta <martuki8tuki@gmail.com>
2024-08-09 13:45:59 +0200

    copy

--- pages/symtomatology.vue
+++ pages/symtomatology.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
+      <ColorScheme><USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" class="w-32 h-10" /></ColorScheme>
+  </div>
+  <div class="flex justify-center">
+    Select from the following list all the symtoms the patient describes
+  </div>
+  <div>
     <UContainer>
     <UCard class="mt-10">
       <template #header>
         <div class="flex justify-between">
-          <h1>Welcome to Nuxt UI Starter</h1>
-          <ColorScheme><USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" /></ColorScheme>
+          Aqui posaré el buscador
         </div>
       </template>
-      <UButton icon="i-heroicons-book-open" to="https://ui.nuxt.com" target="_blank">Open Nuxt UI Documentation</UButton>
+      <SymtomTable></SymtomTable>
     </UCard>
   </UContainer>
   </div>
